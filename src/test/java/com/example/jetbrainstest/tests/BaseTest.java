051c6731837d2c2577dcf3a46bf5e13d30374839
Сергей Серебряков <serzh.serebryakov.96@gmail.com>
2023-07-13 21:50:59 +0700

    optimized code

--- src/test/java/com/example/jetbrainstest/tests/BaseTest.java
+++ src/test/java/com/example/jetbrainstest/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package com.example.jetbrainstest.tests;
 
+import com.example.jetbrainstest.pages.RubyPage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
@@ -13,18 +14,28 @@ public class BaseTest {
 
     @BeforeEach
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+        getDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     public static WebDriver getDriver(){
+        if (driver == null)
+            createDriver();
+        if (driver.toString().contains("(null)"))
+            createDriver();
         return driver;
     }
 
+    private static void createDriver(){
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+    public RubyPage getRubyPage(){
+        return new RubyPage(driver);
+    }
     @AfterEach
     public void tearDown() {
         driver.quit();
--- src/test/java/com/example/jetbrainstest/tests/RubyTest.java
+++ src/test/java/com/example/jetbrainstest/tests/RubyTest.java
@@ -1,47 +1,46 @@
 package com.example.jetbrainstest.tests;
 
 // page_url = https://www.jetbrains.com/ruby/
-import com.example.jetbrainstest.pages.IntellijIdeaPage;
-import com.example.jetbrainstest.pages.PyCharmPage;
-import com.example.jetbrainstest.pages.RubyPage;
+
+import com.example.jetbrainstest.MyExtension;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class RubyTest extends BaseTest {
-    private RubyPage rubyPage;
+ @ExtendWith(MyExtension.class)
+ public class RubyTest extends BaseTest {
 
     @BeforeEach
     @Override
+    @Step
     public void setUp() {
         super.setUp();
         getDriver().get("https://www.jetbrains.com/ruby/");
-        rubyPage = new RubyPage(getDriver());
     }
-
     @Test
     @DisplayName("Проверка, что кнопка скачивания активна")
     public void buttonCheck() {
-        assertTrue(rubyPage.checkIfDownloadButtonIsClickable(), "Кнопка скачивания не активна");
+        assertTrue(getRubyPage().checkIfDownloadButtonIsClickable(), "Кнопка скачивания не активна");
     }
 
     @Test
     @DisplayName("Проверка урла после клика")
     public void checkUrlAfterClickPyCharmButton() {
-        rubyPage = new RubyPage(getDriver());
-        Assertions.assertEquals(rubyPage.clickrubyMineButtonCheckUrl(), "https://www.jetbrains.com/ruby/", "Урл не тот");
+        Assertions.assertEquals(getRubyPage().clickrubyMineButtonCheckUrl(), "https://www.jetbrains.com/ruby/", "Урл не тот");
     }
     @Test
     @DisplayName("Проверяю активность кнопки акции на бесплатные 30 дней")
     public void buttonPromotionCheck() {
-        assertTrue(rubyPage.checkPromotionButtonIsClickable(), "Кнопка акции не активна");
+        assertTrue(getRubyPage().checkPromotionButtonIsClickable(), "Кнопка акции не активна");
         }
     @Test
     @DisplayName("Проверяю видимость первого скриншота")
     public void firstScreenshotIsVisible() {
-        assertTrue(rubyPage.checkFirstScreenshotIsVisible(), "Первый скриншот не виден");
+        assertTrue(getRubyPage().checkFirstScreenshotIsVisible(), "Первый скриншот не виден");
     }
 }
